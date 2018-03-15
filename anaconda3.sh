21bdc306b80f914d9fe627d857156851c7e7c14c
Matt Hodgskiss <matthew.hodgskiss@md.catapult.org.uk>
2018-03-15 14:29:23 +0000

    fixed Spyder not starting

--- anaconda3.sh
+++ anaconda3.sh
@@ -1,18 +1,17 @@
-echo "Guide based on https://www.digitalocean.com/community/tutorials/how-to-install-the-anaconda-python-distribution-on-ubuntu-16-04"
 cd /tmp
 curl -O https://repo.continuum.io/archive/Anaconda3-4.2.0-Linux-x86_64.sh
 bash Anaconda3-4.2.0-Linux-x86_64.sh -b
-PATH=~/anaconda3/bin:$PATH
+export PATH=~/anaconda3/bin:$PATH
 source ~/.bashrc
-conda remove -y qt
-conda remove -y pyqt
-conda install -y qt
-conda install -y pyqtcd ..
-conda list
+#conda remove -y qt
+#conda remove -y pyqt
+#conda install -y qt
+#conda install -y pyqtcd ..
+#conda list
 conda create -y --name my_env python=3
 source activate my_env
-conda info --envs
-conda install -y --name my_env numpy
-cd ~/anaconda3/bin
+#conda info --envs
+#conda install -y --name my_env numpy
+#cd ~/anaconda3/bin
 
 

bcca3d2f8ff0df5a96c25d7bcd492d96c6c03ae6
Matt Hodgskiss <matthew.hodgskiss@md.catapult.org.uk>
2018-03-15 13:36:33 +0000

    Updated bug with anaconda install

--- anaconda3.sh
+++ anaconda3.sh
@@ -1,14 +1,18 @@
+echo "Guide based on https://www.digitalocean.com/community/tutorials/how-to-install-the-anaconda-python-distribution-on-ubuntu-16-04"
 cd /tmp
 curl -O https://repo.continuum.io/archive/Anaconda3-4.2.0-Linux-x86_64.sh
-bash Anaconda3-4.2.0-Linux-x86_64.sh
+bash Anaconda3-4.2.0-Linux-x86_64.sh -b
+PATH=~/anaconda3/bin:$PATH
 source ~/.bashrc
+conda remove -y qt
+conda remove -y pyqt
+conda install -y qt
+conda install -y pyqtcd ..
 conda list
-conda create --name my_env python=3
+conda create -y --name my_env python=3
 source activate my_env
 conda info --envs
-conda install --name my_env numpy
-cd $HOME/anaconda3
-sudo ./conda remove qt
-sudo ./conda remove pyqt
-sudo ./conda install qt
-sudo ./conda install pyqt
+conda install -y --name my_env numpy
+cd ~/anaconda3/bin
+
+

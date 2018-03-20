4246a6f1804e324ddf2a8d3f4a928c21888602a9
matt <Matthew.Hodgskiss@md.catapult.org.uk>
2018-03-20 17:10:16 +0000

    Added Kibana commands

--- docker/docker-kibana.sh
+++
@@ -1,2 +0,0 @@
-docker pull docker.elastic.co/kibana/kibana:6.2.2
-docker run --name some-kibana --link some-elasticsearch:elasticsearch -p 5601:5601 -d kibana
--- docker/elastic_search/docker-elastic-search.sh
+++ docker/elastic_search/docker-elastic-search.sh
@@ -1,2 +1,2 @@
 docker pull docker.elastic.co/elasticsearch/elasticsearch:6.2.2
-docker run -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" docker.elastic.co/elasticsearch/elasticsearch:6.2.2
+docker run --network=elastic --name some-elasticsearch -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" docker.elastic.co/elasticsearch/elasticsearch:6.2.2
